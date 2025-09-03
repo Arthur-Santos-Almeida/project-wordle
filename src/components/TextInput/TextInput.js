6176ead2e841d71863296ab1e964fc45d259c5b1
Arthur Santos Almeida <arthur_santosazv@outlook.com>
2025-09-03 00:17:14 -0300

    Exercise 1 tide up

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,23 +1,31 @@
 import React from "react";
 
 function TextInput({ labelContent, guess, setGuess }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(guess);
+
+    setGuess("");
+  }
+
   return (
     <>
       <form
         className="guess-input-wrapper"
-        onSubmit={(event) => {
-          event.preventDefault();
-          console.log(guess.toUpperCase());
-          setGuess("");
-        }}
+        onSubmit={(event) => handleSubmit(event)}
       >
         <label htmlFor="guess-input">{labelContent}</label>
         <input
           id="guess-input"
           type="text"
           value={guess}
-          onChange={(event) => setGuess(event.target.value)}
+          onChange={(event) => {
+            const nextGuess = event.target.value.toUpperCase();
+            setGuess(nextGuess);
+          }}
           pattern="\w{5}"
+          title="Please enter a 5-letter word"
         ></input>
       </form>
     </>
