4a75caf0c1327d5e004823db31d70ad5ade6278f
Arthur Santos Almeida <arthur_santosazv@outlook.com>
2025-09-02 23:48:46 -0300

    Exercise 1

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
+import TextInput from "../TextInput";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +10,15 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState("");
+
+  return (
+    <TextInput
+      guess={guess}
+      setGuess={setGuess}
+      labelContent={"Enter guess:"}
+    ></TextInput>
+  );
 }
 
 export default Game;
